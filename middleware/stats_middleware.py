7aae7126db03ab0e77afa9e03bf5808f8f406c6f
Harish Mallipeddi <harish.mallipeddi@gmail.com>
2008-09-02 15:32:54 +0800

    minor bug fix in stats middleware

--- middleware/stats_middleware.py
+++ middleware/stats_middleware.py
@@ -7,21 +7,24 @@ class PipesStatsMiddleware:
         if settings.DEBUG:
             
             queries = pipes.debug_stats.queries
-            cached_queries = filter(lambda query: query['found_in_cache'], queries)
-            failed_queries = filter(lambda query: query['failed'], queries)
             
-            print "\n================== Pipes Usage Summary ==========================="
-            print "Total: %d   Found in cache: %d   Fetched from remote: %d   Failed: %d\n" % (
-                    len(queries), len(cached_queries),
-                    len(queries) - len(cached_queries), len(failed_queries)
-                )
-            for idx, query in enumerate(queries):
-                if query['failed']:
-                    status = "FAILED"
-                elif query['found_in_cache']:
-                    status = "FETCHED FROM CACHE"
-                else:
-                    status = "FETCHED FROM REMOTE"
-                print "%d) %s : %s : %d retries" % (idx+1, status, query['url'], query['retries'])
-            print "====================================================================\n"
+            if len(queries) > 0:
+                cached_queries = filter(lambda query: query['found_in_cache'], queries)
+                failed_queries = filter(lambda query: query['failed'], queries)
+            
+                print "\n================== Pipes Usage Summary ==========================="
+                print "Total: %d   Found in cache: %d   Fetched from remote: %d   Failed: %d\n" % (
+                        len(queries), len(cached_queries),
+                        len(queries) - len(cached_queries) - len(failed_queries), len(failed_queries)
+                    )
+                for idx, query in enumerate(queries):
+                    if query['failed']:
+                        status = "FAILED"
+                    elif query['found_in_cache']:
+                        status = "FETCHED FROM CACHE"
+                    else:
+                        status = "FETCHED FROM REMOTE"
+                    print "%d) %s : %s : %d retries" % (idx+1, status, query['url'], query['retries'])
+                print "====================================================================\n"
+        
         return response
